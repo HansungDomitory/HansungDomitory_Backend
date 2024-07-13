eedf628c349159b3cc2f488820a0919fbd2f9479
ssarisong <thdtnwls2989@naver.com>
2024-07-13 13:32:55 +0900

    Add ApplyOut Entity

--- src/apis/applyOut/entities/applyOut.entity.ts
+++ src/apis/applyOut/entities/applyOut.entity.ts
@@ -1,3 +1,34 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class ApplyOut {
-    
+
+    @ApiProperty({ description: "고유번호" })
+    @PrimaryGeneratedColumn('increment')
+    id: string;
+
+    @ApiProperty({ description: "외박 신청 학생 학번" })
+    @Column()
+    student_id: string;
+
+    @ApiProperty({ description: "외박일수" })
+    @Column()
+    duration: number;
+
+    @ApiProperty({ description: "시작 날짜" })
+    @Column()
+    start_date: Date;
+
+    @ApiProperty({ description: "종료 날짜" })
+    @Column()
+    end_date: Date;
+
+    @ApiProperty({ description: "내용" })
+    @Column()
+    detail: string;
+
+    @ApiProperty({ description: "생성일자" })
+    @CreateDateColumn()
+    create_at: Date;
 }
